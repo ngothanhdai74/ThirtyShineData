a684d6ac42ec0a9754c9801ce2e9046edfc93139
ngo dai <ngothanhdai74@gmail.com>
2020-10-08 15:36:04 +0700

    fd

--- ThirtyShine.InitData/Processing.cs
+++ ThirtyShine.InitData/Processing.cs
@@ -11,25 +11,38 @@ namespace ThirtyShine.InitData
     {
         public async Task Handler()
         {
-            using(var db = new _20190809Context())
+            try
             {
-                db.PermissionMenuAction.Add(new PermissionMenuAction()
-                {
-                    PermissionId = 7,
-                    ActionId = 1,
-                    PageId = 20245,
-                    CreatedTime = DateTime.UtcNow,
-                    IsActive = true,
-                    IsDelete = false,
-                });
-                //var data = db.PermissionMenu.FirstOrDefault(m => m.Name.Contains("Điểm danh"));
-                //if (data != null)
-                //{
-                //    data.Pid = 20245;
-
-                    var res = db.SaveChanges();
-                //}
+                var tzis = TimeZoneInfo.GetSystemTimeZones();
+                TimeZoneInfo cstZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
+                var convert = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, cstZone);
+                var bw = DateTime.Now;
             }
+            catch (Exception ex)
+            {
+
+                Console.ReadKey();
+            }
+
+            //using(var db = new _20190809Context())
+            //{
+            //    db.PermissionMenuAction.Add(new PermissionMenuAction()
+            //    {
+            //        PermissionId = 7,
+            //        ActionId = 1,
+            //        PageId = 20245,
+            //        CreatedTime = DateTime.UtcNow,
+            //        IsActive = true,
+            //        IsDelete = false,
+            //    });
+            //    //var data = db.PermissionMenu.FirstOrDefault(m => m.Name.Contains("Điểm danh"));
+            //    //if (data != null)
+            //    //{
+            //    //    data.Pid = 20245;
+            //        var res = db.SaveChanges();
+            //    //}
+            //}
+            Console.ReadKey();
         }
     }
     public interface IProcessing
