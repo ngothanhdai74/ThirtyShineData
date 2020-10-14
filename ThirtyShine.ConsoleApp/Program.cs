046db410ca7321d2a059286274f73828c65e0138
ngo dai <ngothanhdai74@gmail.com>
2020-10-14 11:22:45 +0700

    vfg

---
+++ ThirtyShine.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ThirtyShine.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #if DEBUG
+                Console.WriteLine("Mode=Debug");
+            #else
+                Console.WriteLine("Mode=Release"); 
+            #endif
+        }
+    }
+}
