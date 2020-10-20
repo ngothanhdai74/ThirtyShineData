850440983dc873b288f9be592a06a992a91d6751
ngo dai <ngothanhdai74@gmail.com>
2020-10-20 10:29:52 +0700

    j

--- ThirtyShine.InitData/S4MRepository.cs
+++ ThirtyShine.InitData/S4MRepository.cs
@@ -13,19 +13,24 @@ namespace ThirtyShine.InitData
         {
             using (var db = new S4MContext())
             {
-                var query = from bill in db.BillService.Where(m => m.CreatedDate >= DateTime.Now.AddDays(-40))
-                            join cus in db.Customer on bill.CustomerId equals cus.Id
-                            join salon in db.TblSalon.Where(m => m.IsSalonHoiQuan == true) on bill.SalonId equals salon.Id
-                            select new
-                            {
-                                idKh = cus.Id,
-                                nameKh = cus.Fullname,
-                                sdt = cus.Phone,
-                                salonlastbill = salon.Id,
-                                datelastbill = bill.CompleteBillTime
-                            };
-
-                var data = query.ToSql();
+                var data = db.ScriptData.Add(new S4M.Table.ScriptData()
+                {
+                    Name = @"[S4M] danh sach thong tin khach hang su dung dich vu lan cuoi cach day 40 ngay.sql",
+                    File = @"C:\Deploy\erp-v4.30shine.com\PublishOutput\[S4M] danh sach thong tin khach hang su dung dich vu lan cuoi cach day 40 ngay.sql"
+                });
+                var sa = db.SaveChanges();
+                //var query = from bill in db.BillService.Where(m => m.CreatedDate >= DateTime.Now.AddDays(-40))
+                //            join cus in db.Customer on bill.CustomerId equals cus.Id
+                //            join salon in db.TblSalon.Where(m => m.IsSalonHoiQuan == true) on bill.SalonId equals salon.Id
+                //            select new
+                //            {
+                //                idKh = cus.Id,
+                //                nameKh = cus.Fullname,
+                //                sdt = cus.Phone,
+                //                salonlastbill = salon.Id,
+                //                datelastbill = bill.CompleteBillTime
+                //            };
+                // data = query.ToSql();
                 return;
             }
         }
