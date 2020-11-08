060fd41aea2f259d4c992349005309533f672f19
ngo dai <ngothanhdai74@gmail.com>
2020-11-08 23:16:12 +0700

    f

---
+++ Repository/Implements/IvInventoryCurrent.cs
@@ -0,0 +1,14 @@
+﻿using DB201990809;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Implements
+{
+    public class IvInventoryCurrentRepo : BaseRepository<_20190809Context, IvInventoryCurrent>, IIvInventoryCurrent
+    {
+        public IvInventoryCurrentRepo(_20190809Context context) : base(context){}
+
+    }
+}
