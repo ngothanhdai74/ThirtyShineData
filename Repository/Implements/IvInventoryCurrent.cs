461095a3f17a551aa371511a4f60dc05a9af893b
ngo dai <ngothanhdai74@gmail.com>
2020-11-08 23:28:49 +0700

    s

--- Repository/Implements/IvInventoryCurrent.cs
+++ Repository/Implements/IvInventoryCurrent.cs
@@ -2,13 +2,38 @@
 using Repository.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Uitility.Common;
 
 namespace Repository.Implements
 {
     public class IvInventoryCurrentRepo : BaseRepository<_20190809Context, IvInventoryCurrent>, IIvInventoryCurrent
     {
-        public IvInventoryCurrentRepo(_20190809Context context) : base(context){}
+        public IvInventoryCurrentRepo(_20190809Context context) : base(context) { }
+
+        public async Task Process()
+        {
+            var pageIndex = 0;
+            var pageSize = 100;
+            var paging = new Paging(pageIndex, pageSize);
+            var data = await QueryAsync(paging);
+
+            if (data?.Any() == true)
+            {
+                var totalPage = paging.TotalRow / pageSize + (paging.TotalRow % pageSize == 0 ? 0 : 1);
+                while (pageIndex < totalPage)
+                {
+                    // handler data
+
+                    //
+                    pageIndex++;
+                    paging = new Paging(pageIndex, pageSize);
+                    data = await QueryAsync(paging);
+                }
+            }
+        }
 
     }
 }
