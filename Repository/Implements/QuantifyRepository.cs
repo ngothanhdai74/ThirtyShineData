b50c49a3aedf75df5b94058e5019e365f059b7db
ngo dai <ngothanhdai74@gmail.com>
2020-11-18 09:50:25 +0700

    dsa

--- Repository/Implements/QuantifyRepository.cs
+++ Repository/Implements/QuantifyRepository.cs
@@ -371,6 +371,297 @@ namespace Repository.Implements
                 {
                     try
                     {
+                        var products = db.Product.Where(m => m.IsDelete == 0).OrderBy(m => m.Id).Take(10).ToList();
+                        var services = db.Service.Where(m => m.IsDelete == 0).OrderBy(m => m.Id).Take(2).ToList();
+                        var salonIdcho = db.IvInventory.FirstOrDefault(n => n.Type == 2).SalonId;
+                        var salonId = db.TblSalon.FirstOrDefault(m => salonIdcho == m.Id).Id;
+                        var productModels = ProductModel.GetMock();
+                        var inventory = db.IvInventory.FirstOrDefault(m => m.SalonId == salonId);
+                        var inventoryId = inventory.Id;
+                        var inventoryPartnerId = inventory.ParentId;
+                        #region Step 1
+                        // create data BillServiceHis and FlowService
+                        string notebillServiceHis = "bill de test daint";
+                        //======================================================
+                        var billServiceHisSearch = db.BillServiceHis.Where(m => m.ImageChecked1.Equals(notebillServiceHis) && m.SalonId == salonId && m.IsDelete == 0).ToList();
+                        //--
+                        if (billServiceHisSearch.Any())
+                        {
+                            db.BillServiceHis.RemoveRange(billServiceHisSearch);
+                            db.SaveChanges();
+                        }
+                        for (int i = 0; i < 5; i++)
+                        {
+                            db.BillServiceHis.Add(new BillServiceHis()
+                            {
+                                Id = 10000000 + i,
+                                SalonId = salonId,
+                                CompleteBillTime = DateTime.Now.AddHours(-(i)),
+                                ImageChecked1 = notebillServiceHis,
+                                IsDelete = 0,
+                                CreatedDate = DateTime.UtcNow.Date
+                            });
+                        }
+                        var res = db.SaveChanges();
+                        if (res <= 0)
+                        {
+                            throw new Exception();
+                        }
+                        billServiceHisSearch = db.BillServiceHis.Where(m => m.ImageChecked1.Equals(notebillServiceHis) && m.IsDelete == 0 && m.SalonId == salonId && m.CreatedDate == DateTime.UtcNow.Date).ToList();
+                        //----------------------------------------------------------------------------------------------------------------------------------------------
+                        foreach (var item in billServiceHisSearch)
+                        {
+                            db.FlowService.Add(new FlowService()
+                            {
+                                BillId = item.Id,
+                                ServiceId = services[0].Id,
+                                IsDelete = 0,
+                                CreatedDate = DateTime.UtcNow.Date
+                            });
+                            db.FlowService.Add(new FlowService()
+                            {
+                                BillId = item.Id,
+                                ServiceId = services[1].Id,
+                                IsDelete = 0,
+                                CreatedDate = DateTime.UtcNow.Date
+                            });
+                        }
+                        res = db.SaveChanges();
+                        if (res <= 0)
+                        {
+                            throw new Exception();
+                        }
+                        #endregion
+
+                        #region Step 2
+                        string groupQuantifyTest = "group quantify daint test ";
+                        // mock group quantify
+                        db.IvGroupQuantifyV2.RemoveRange(db.IvGroupQuantifyV2.ToList()); db.SaveChanges();
+                        for (int i = 1; i <= 5; i++)
+                        {
+                            db.IvGroupQuantifyV2.Add(new IvGroupQuantifyV2()
+                            {
+                                IsDelete = 0,
+                                GroupName = groupQuantifyTest + i,
+                                CreatedDate = DateTime.UtcNow.Date
+                            });
+                        }
+                        res = db.SaveChanges();
+                        if (res <= 0)
+                        {
+                            throw new Exception();
+                        }
+                        // mock service quantify
+                        var groupQuantifies = db.IvGroupQuantifyV2.Where(m => m.IsDelete == 0 && m.GroupName.Contains(groupQuantifyTest) && m.CreatedDate == DateTime.UtcNow.Date).ToList();
+                        //
+                        db.IvServiceQuantifyV2.RemoveRange(db.IvServiceQuantifyV2.ToList()); db.SaveChanges();
+                        foreach (var item in services)
+                        {
+                            foreach (var subitem in groupQuantifies)
+                            {
+                                db.IvServiceQuantifyV2.Add(new IvServiceQuantifyV2()
+                                {
+                                    GroupQuantifyId = subitem.Id,
+                                    IsDelete = 0,
+                                    ServiceId = item.Id,
+                                    CreatedDate = DateTime.UtcNow.Date
+                                });
+                            }
+                        }
+                        res = db.SaveChanges();
+                        if (res <= 0)
+                        {
+                            throw new Exception();
+                        }
+                        var serviceQuantifies = db.IvServiceQuantifyV2.Where(m => m.CreatedDate == DateTime.UtcNow.Date && m.IsDelete == 0 & services.Select(m => m.Id).ToList().Contains((int)m.ServiceId)).ToList();
+                        // mock product quantify
+                        db.IvProductQuantifyV2.RemoveRange(db.IvProductQuantifyV2.ToList()); db.SaveChanges();
+                        List<IvProductQuantifyV2> mo = new List<IvProductQuantifyV2>();
+                        foreach (var product in products)
+                        {
+                            mo.Add(new IvProductQuantifyV2()
+                            {
+                                ProductId = product.Id,
+                                Volume = 3,
+                                Quantify = 1,
+                                IsBase = 0,
+                                IsDelete = 0,
+                                CreatedDate = DateTime.UtcNow.Date
+                            });
+                        }
+                        for (int i = 0; i < products.Count; i = i + 2)
+                        {
+                            mo[i].IsBase = 1;
+                        }
+                        db.IvProductQuantifyV2.AddRange(mo); db.SaveChanges();
+                        //
+                        var productQuantifies = db.IvProductQuantifyV2.Where(m => products.Select(m => m.Id).ToList().Contains((int)m.ProductId) && m.CreatedDate == DateTime.UtcNow.Date && m.IsDelete == 0).ToList();
+                        // mock group quantify product 
+                        int ii = 0;
+                        db.IvGroupQuantifyProductV2.RemoveRange(db.IvGroupQuantifyProductV2.ToList());
+                        db.SaveChanges();
+                        foreach (var groupQuantify in groupQuantifies)
+                        {
+                            if (products.Count >= ii + 2)
+                            {
+                                db.IvGroupQuantifyProductV2.Add(new IvGroupQuantifyProductV2()
+                                {
+                                    IsDelete = 0,
+                                    GroupQuantifyId = groupQuantify.Id,
+                                    CreatedDate = DateTime.UtcNow.Date,
+                                    ProductQuantifyId = products[ii++].Id
+                                });
+                                db.IvGroupQuantifyProductV2.Add(new IvGroupQuantifyProductV2()
+                                {
+                                    IsDelete = 0,
+                                    GroupQuantifyId = groupQuantify.Id,
+                                    CreatedDate = DateTime.UtcNow.Date,
+                                    ProductQuantifyId = products[ii++].Id
+                                });
+                            }
+                        }
+                        res = db.SaveChanges();
+                        if (res <= 0)
+                        {
+                            throw new Exception();
+                        }
+                        var productQuantifyGroup = db.IvGroupQuantifyProductV2.Where(m => products.Select(m => m.Id).ToList().Contains((int)m.ProductQuantifyId) && m.CreatedDate == DateTime.UtcNow.Date && m.IsDelete == 0).ToList();
+                        // mock inventory current
+                        var ivCu = db.IvInventoryCurrent.Where(m => products.Select(m => m.Id).ToList().Contains(m.ProductId) && m.IsDelete == false && m.InventoryId == inventoryId).ToList();
+                        if (ivCu?.Any() == true)
+                        {
+                            db.IvInventoryCurrent.RemoveRange(ivCu);
+                            res = db.SaveChanges();
+                            if (res <= 0)
+                            {
+                                throw new Exception();
+                            }
+                        }
+                        ii = 0;
+                        var modelMock = InventoryCurrentMock.GetData();
+                        if (modelMock.Count == products.Count)
+                        {
+                            foreach (var product in products)
+                            {
+                                db.IvInventoryCurrent.Add(new IvInventoryCurrent()
+                                {
+                                    InventoryId = inventoryId,
+                                    ProductId = product.Id,
+                                    Begin = modelMock[ii].Begin,
+                                    SellOrUse = modelMock[ii].SellOrUse,
+                                    Import = modelMock[ii].Import,
+                                    Export = modelMock[ii].Export,
+                                    CreatedDate = DateTime.UtcNow.Date,
+                                    IsDelete = false
+                                });
+                                ii++;
+                                res = db.SaveChanges();
+                                if (res <= 0)
+                                {
+                                    throw new Exception();
+                                }
+                            }
+                        }
+                        ivCu = db.IvInventoryCurrent.Where(m => products.Select(m => m.Id).ToList().Contains(m.ProductId) && m.InventoryId == inventoryId && m.IsDelete == false && m.CreatedDate == DateTime.UtcNow.Date).ToList();
+                        // mock QuantifyChosen
+                        foreach (var product in products)
+                        {
+                            db.IvQuantifyChosen.Add(new IvQuantifyChosen()
+                            {
+                                ProductId = product.Id
+                            });
+                        }
+                        res = db.SaveChanges();
+                        if (res <= 0)
+                        {
+                            throw new Exception();
+                        }
+                        var productChosen = db.IvQuantifyChosen.Where(m => products.Select(m => m.Id).ToList().Contains(m.ProductId)).ToList();
+                        //-----------------------------------------------------------------------------------------------------------------------------
+                        #endregion
+                        await transaction.CommitAsync();
+                    }
+                    catch (Exception ex)
+                    {
+                        await transaction.RollbackAsync();
+                    }
+                }
+            }
+        }
+        private async static Task DeleteData()
+        {
+            using (var db = new Solution30ShineContext())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var products = db.Product.Where(m => m.IsDelete == 0).OrderBy(m => m.Id).Take(10).ToList();
+                        var services = db.Service.Where(m => m.IsDelete == 0).OrderBy(m => m.Id).Take(2).ToList();
+                        var salonIdcho = db.IvInventory.FirstOrDefault(n => n.Type == 2).SalonId;
+                        var salonId = db.TblSalon.FirstOrDefault(m => salonIdcho == m.Id).Id;
+                        var productModels = ProductModel.GetMock();
+                        var inventory = db.IvInventory.FirstOrDefault(m => m.SalonId == salonId);
+                        var inventoryId = inventory.Id;
+                        var inventoryPartnerId = inventory.ParentId;
+                        //
+                        var productChosen = db.IvQuantifyChosen.Where(m => products.Select(m => m.Id).ToList().Contains(m.ProductId)).ToList();
+                        db.IvQuantifyChosen.RemoveRange(productChosen);
+                        var res = db.SaveChanges();
+                        if (res <= 0)
+                        {
+                            throw new Exception();
+                        }
+                        var ivCu = db.IvInventoryCurrent.Where(m => products.Select(m => m.Id).ToList().Contains(m.ProductId) && m.InventoryId == inventoryId && m.IsDelete == false && m.CreatedDate == DateTime.UtcNow.Date).ToList();
+                        // mock QuantifyChosen
+                        db.IvInventoryCurrent.RemoveRange(ivCu);
+                        res = db.SaveChanges();
+                        if (res <= 0)
+                        {
+                            throw new Exception();
+                        }
+                        var productQuantifyGroup = db.IvGroupQuantifyProductV2.Where(m => products.Select(m => m.Id).ToList().Contains((int)m.ProductQuantifyId) && m.CreatedDate == DateTime.UtcNow.Date && m.IsDelete == 0).ToList();
+                        res = db.SaveChanges();
+                        if (res <= 0)
+                        {
+                            throw new Exception();
+                        }
+                        var productQuantifies = db.IvProductQuantifyV2.Where(m => products.Select(m => m.Id).ToList().Contains((int)m.ProductId) && m.CreatedDate == DateTime.UtcNow.Date && m.IsDelete == 0).ToList();
+                        db.IvProductQuantifyV2.RemoveRange(productQuantifies);
+                        res = db.SaveChanges();
+                        if (res <= 0)
+                        {
+                            throw new Exception();
+                        }
+                        var serviceQuantifies = db.IvServiceQuantifyV2.Where(m => m.CreatedDate == DateTime.UtcNow.Date && m.IsDelete == 0 & services.Select(m => m.Id).ToList().Contains((int)m.ServiceId)).ToList();
+                        db.IvServiceQuantifyV2.RemoveRange(serviceQuantifies);
+                        res = db.SaveChanges();
+                        if (res <= 0)
+                        {
+                            throw new Exception();
+                        }
+                        string groupQuantifyTest = "group quantify daint test ";
+                        var groupQuantifies = db.IvGroupQuantifyV2.Where(m => m.IsDelete == 0 && m.GroupName.Contains(groupQuantifyTest) && m.CreatedDate == DateTime.UtcNow.Date).ToList();
+                        db.IvGroupQuantifyV2.RemoveRange(groupQuantifies);
+                        res = db.SaveChanges();
+                        if (res <= 0)
+                        {
+                            throw new Exception();
+                        }
+                        string notebillServiceHis = "bill de test daint";
+                        //======================================================
+                        var billServiceHisSearch = db.BillServiceHis.Where(m => m.ImageChecked1.Equals(notebillServiceHis) && m.SalonId == salonId && m.IsDelete == 0).ToList();
+
+                        var flowServicesSearch = db.FlowService.Where(m => billServiceHisSearch.Select(m => m.Id).ToList().Contains((int)m.BillId)).ToList();
+
+
+                        //--
+                        if (billServiceHisSearch.Any())
+                        {
+                            db.FlowService.RemoveRange(flowServicesSearch);
+                            db.BillServiceHis.RemoveRange(billServiceHisSearch);
+                            db.SaveChanges();
+                        }
                         await transaction.CommitAsync();
                     }
                     catch (Exception ex)
