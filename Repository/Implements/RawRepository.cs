99b0698bf1fe3e9aa70ac7d188a5fd3300c5e257
ngo dai <ngothanhdai74@gmail.com>
2020-11-19 09:52:25 +0700

    d

---
+++ Repository/Implements/RawRepository.cs
@@ -0,0 +1,20 @@
+﻿using Repository.Database.Default;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implements
+{
+    public class RawRepository
+    {
+        public static async Task Test()
+        {
+            using(var db = new Solution30ShineContext())
+            {
+                string sql = @"select top 100 * from IvOrder where Note = 'Auto Quantify Supplies'";
+
+            }
+        } 
+    }
+}
