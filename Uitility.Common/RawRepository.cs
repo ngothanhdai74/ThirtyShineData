b5a658cab7073c8903d1739aba4acb8792b56b47
ngo dai <ngothanhdai74@gmail.com>
2020-11-20 09:33:37 +0700

    fd

---
+++ Uitility.Common/RawRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Uitility.Common
+{
+    public class BaseRawRepository
+    {
+        private readonly string ConnectionString;
+        public BaseRawRepository(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+        private string GetConnectionString(string server = "10.0.2.76", string db = "20190809", string userId = "ad_30s", string password = "VDrs8XHyz3Rdc7PAiv5Q")
+        {
+            SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+            stringBuilder["Server"] = server;
+            stringBuilder["Database"] = db;
+            stringBuilder["User Id"] = userId;
+            stringBuilder["Password"] = password;
+            return stringBuilder.ToString();
+        }
+    }
+}
