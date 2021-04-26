4a4e13d20f82d74691fc498e32de37799ae42137
ngo dai <ngothanhdai74@gmail.com>
2021-04-26 14:06:04 +0700

    s

---
+++ Repository/Implements/StatisticDataRepository.cs
@@ -0,0 +1,23 @@
+﻿using Repository.Database.Default;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implements
+{
+    public class StatisticDataRepository
+    {
+        public async Task CreateBookingAndCompleteBill(int salonId, int bookingCount)
+        {
+            using (var db = new Solution30ShineContext())
+            {
+                //var aa = DateTime.Today.DayOfWeek;
+                //db.TimekeepingSuggestionStaticData
+
+
+
+            }
+        }
+    }
+}
